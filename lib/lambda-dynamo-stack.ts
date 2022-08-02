9ccd0b0b8fdd3eb97da6e117722c01c7dc34d1d8
yomex4life <yomex4life2001@gmail.com>
2022-08-02 09:54:35 +0100

    Start of pipelining

--- lib/lambda-dynamo-stack.ts
+++ lib/lambda-dynamo-stack.ts
@@ -1,16 +1,52 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { NodejsFunctionProps, NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { join } from 'path';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LambdaDynamoStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const productTable = new Table(this, 'product', {
+      partitionKey: {name: 'id', type: AttributeType.STRING},
+      tableName: 'product',
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaDynamoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const nodeJsProductFunctionProps: NodejsFunctionProps = {
+      bundling: {
+      externalModules: [
+          'aws-sdk'
+      ]
+      },
+      environment: {
+          PRIMARY_KEY: 'id',
+          DYNAMODB_TABLE_NAME: productTable.tableName,
+      },
+      runtime: Runtime.NODEJS_16_X
+    } 
+
+    const productFunction = new NodejsFunction(this, 'product-lambda-function', {
+      entry: join(__dirname, `/../src/product/index.js`),
+      ...nodeJsProductFunctionProps
+    });
+
+    productTable.grantReadWriteData(productFunction);
+
+    const api = new LambdaRestApi(this, 'product-api',{
+      restApiName: 'Product Service',
+      handler: productFunction,
+      proxy: false
+    });
+
+    api.root.addMethod('ANY');
+    const books = api.root.addResource('product');
+    books.addMethod('GET');
+    books.addMethod('POST');
   }
 }
